10a74de1fe6c683ffb196cca88f6f0f633124776
Oleg Popkov <popkovoa@ektaservice.local>
2022-04-15 13:26:22 +0300

    add Users essence

---
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\User\StoreRequest;
+use App\Models\User;
+
+class StoreController extends Controller
+{
+    public function __invoke(StoreRequest $request)
+    {
+        $data = $request->validated();
+
+        User::firstOrCreate($data); // the same as - Category::firstOrCreate(['title'=> $data['title']]);
+
+        return redirect()->route('admin.user.index');
+    }
+}
---
+++ resources/views/admin/user/show.blade.php
@@ -0,0 +1,81 @@
+@extends('admin.layouts.main')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 pr-1">{{ $user->name }}
+                            <a href="{{ route('admin.user.edit', $user->id)}}">
+                                <i class="fas fa-pencil-alt"></i>
+                            </a>
+                            <form action="{{ route('admin.user.delete', $user->id)}}" method="POST"  class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="border-0 bg-transparent">
+                                    <i class="fas fa-trash text-danger" role="button" ></i>
+                                </button>
+                            </form>
+                        </h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Responsive Hover Table</h3>
+
+                                <div class="card-tools">
+                                    <div class="input-group input-group-sm" style="width: 150px;">
+                                        <input type="text" name="table_search" class="form-control float-right"
+                                               placeholder="Search">
+
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-default">
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <tbody>
+                                        <tr>
+                                            <td class="text-center">ID</td>
+                                            <td class="text-center">{{ $user->id }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="text-center">Name</td>
+                                            <td class="text-center">{{ $user->name }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
