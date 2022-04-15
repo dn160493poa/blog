ef88aa978221be1062932feda9882982cc988e99
Oleg Popkov <popkovoa@ektaservice.local>
2022-04-15 09:39:37 +0300

    add atach Category to Post

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -30,14 +30,14 @@
                             @csrf
                             <div class="form-group w-50">
                                 <input type="text" class="form-control" placeholder="Value of title" name="title"
-                                value="{{ old('title')}}">
+                                       value="{{ old('title')}}">
                                 @error('title')
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="form-group">
                                 <textarea id="summernote" name="content">{{old('content')}}</textarea>
-                                @error('title')
+                                @error('content')
                                 <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
@@ -48,10 +48,14 @@
                                         <input type="file" class="custom-file-input" name="main_image">
                                         <label class="custom-file-label">Choose file</label>
                                     </div>
+                                    @error('main_image')
+                                    <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                     <div class="input-group-append">
                                         <span class="input-group-text">Upload</span>
                                     </div>
                                 </div>
+
                             </div>
                             <div class="form-group w-100">
                                 <label>Preview</label>
@@ -60,10 +64,26 @@
                                         <input type="file" class="custom-file-input" name="preview_image">
                                         <label class="custom-file-label">Choose file</label>
                                     </div>
+                                    @error('preview_image')
+                                    <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                     <div class="input-group-append">
                                         <span class="input-group-text">Upload</span>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="form-group w-50">
+                                <label>Choose category</label>
+                                <select class="form-control" name="category_id">
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}"
+                                            {{ $category->id == old('category_id') ? 'selected' : '' }}
+                                        >{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
+                                <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Add">
