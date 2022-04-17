70b72f4d89b552fdf3513c7a6cbba796b0ea4c28
Oleg Popkov <popkovoa@ektaservice.local>
2022-04-17 17:59:53 +0300

    add Blog-single page

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
-        $random_posts = Post::get()->random(4);
-        $liked_posts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
-        return view('main.index', compact('posts', 'random_posts', 'liked_posts'));
+        return redirect()->route('post.index');
     }
 
 }
