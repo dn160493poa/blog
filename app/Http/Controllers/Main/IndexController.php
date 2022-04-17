664f76522a9eeaf148eae0a7679552621a303a4d
Oleg Popkov <popkovoa@ektaservice.local>
2022-04-17 15:14:53 +0300

    add Posts and pagination to main page

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('main.index', compact('data'));
+        $posts = Post::paginate(6);
+        $random_posts = Post::get()->random(4);
+        $liked_posts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
+        return view('main.index', compact('posts', 'random_posts', 'liked_posts'));
     }
 
 }
